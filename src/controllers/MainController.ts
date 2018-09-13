e7f71e07d975e2d88d02dbc9168661c4b58e6401
Maciek Adamus <melonik0.9846@gmail.com>
2018-09-13 18:28:49 +0200

    Preparation to extract front to different repo

--- src/controllers/MainController.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from 'routing-controllers'
-
-@Controller()
-export class MainController {
-    @Get('/')
-    main() {
-        return "Click <a href=\"/auth/login\">here</a> to log in with Spotify"
-    }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import 'reflect-metadata'
 
 import { createExpressServer } from 'routing-controllers'
+import { static as staticServing } from 'express'
 import bodyParser from 'body-parser'
 import compression from 'compression'
 import helmet from 'helmet'
 import dotenv from 'dotenv'
+import { Express } from 'express'
 
 import controllers from 'backend/controllers'
 import { sessionMiddleware } from 'backend/middleware'
 
 dotenv.config()
 
-const app = createExpressServer({
+const app: Express = createExpressServer({
     controllers
 })
 
@@ -22,5 +24,6 @@ app.use(compression())
 
 export const STATE_KEY = 'spoti_authentication_state'
 export const SCOPES = ['user-read-email', 'user-read-private', 'user-read-playback-state', 'user-modify-playback-state', 'user-read-currently-playing']
+export const IS_DEV_ENV = app.get('env') !== 'production'
 
 export default app
