20210094d17602bb2dcf6bcf2600e9158f35291c
Mafciejewicz <melonik0.9846@gmail.com>
2018-10-16 00:21:55 +0200

    Fixed ts-node shit error about default export

--- src/services/SpotifyService/connect.ts
+++ src/services/SpotifyService/connect.ts
@@ -1,5 +1,6 @@
-import SpotifyApi from 'spotify-web-api-node'
+import * as SpotifyApi from 'spotify-web-api-node'
 
+// @ts-ignore
 export const api = new SpotifyApi({
     clientId: process.env.SPOTIFY_APP_ID,
     clientSecret: process.env.SPOTIFY_APP_SECRET,
